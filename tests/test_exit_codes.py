15350c962527e3163ea4f6189c52a117d413df22
Austin Bingham <austin.bingham@gmail.com>
2018-04-02 16:14:22 +0200

    Using a pytest fixture to provide all `os` exit codes.
    We loop over these codes in several places.

--- tests/test_exit_codes.py
+++ tests/test_exit_codes.py
@@ -1,9 +1,15 @@
-# There's not much to test. Verifying the the module imports cleanly is
-# pretty good. That plus accessing a few attributes will verify that it's
-# cross-platform.
-
 import os
 import exit_codes
+
+import pytest
+
+
+@pytest.fixture(params=[name[3:]
+                        for name in dir(os)
+                        if name.startswith('EX')])
+def os_code_name(request):
+    "All return code names (sans EX_) from `os`."
+    return request.param
 
 
 def test_ok():
@@ -14,13 +20,9 @@ def test_usage():
     return exit_codes.ExitCode.USAGE
 
 
-def test_has_same_attributes_as_os():
-    os_exit_codes_names = (name[3:] for name in dir(os) if name.startswith('EX'))
-    for name in os_exit_codes_names:
-        assert name in exit_codes.ExitCode.__members__
+def test_has_same_attributes_as_os(os_code_name):
+    assert os_code_name in exit_codes.ExitCode.__members__
 
 
-def test_has_same_values_as_os():
-    os_exit_codes_names = (name[3:] for name in dir(os) if name.startswith('EX'))
-    for name in os_exit_codes_names:
-        assert getattr(exit_codes.ExitCode, name) == getattr(os, 'EX_' + name)
+def test_has_same_values_as_os(os_code_name):
+    assert getattr(exit_codes.ExitCode, os_code_name) == getattr(os, 'EX_' + os_code_name)
